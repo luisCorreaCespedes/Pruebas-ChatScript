1c4d6c0e54cb025f4caee261d2b7ca2427ae9086
Luis Correa Céspedes <luiscorreacespedes@gmail.com>
2022-06-24 04:30:16 -0400

    Timer.py añadido

--- RAWDATA/INBOUND/SCRIPTS/CSVtoTXT.py
+++ RAWDATA/INBOUND/SCRIPTS/CSVtoTXT.py
@@ -1,16 +1,27 @@
-#Importación archivo .csv para lectura
+#Librerías
 import csv
-entrada = open("../ARCHIVO_CARGA/Base-Cocacola-17-06-2022.csv")
-datos = csv.reader(entrada)
+import os
 
-#Exportación archivo .txt con datos del .csv
-salida = open("../DATOS/datos.txt", "w", encoding="utf-8")
-for fila in datos:
-    for columna in fila:
-        salida.write(columna)
-        salida.write("\t")
-    salida.write("\n")
-    
+def ejecucionScript():
 
-salida.close()
-entrada.close()
+    #Carga de ficheros
+    directorio = "../ARCHIVO_CARGA/"
+    with os.scandir(directorio) as ficheros:
+        ficheros = [fichero.name for fichero in ficheros if fichero.is_file() and fichero.name.endswith('.csv')]
+
+    #Lectura de ficheros
+    for rutas in ficheros:
+        entrada = open("../ARCHIVO_CARGA/" + rutas, encoding="utf-8")
+        datos = csv.reader(entrada)
+
+        #Exportación fichero .txt con datos del .csv
+        salida = open("../DATOS/datos.txt", "a", encoding="utf-8")
+        for fila in datos:
+            for columna in fila:
+                salida.write(columna)
+                salida.write("\t")
+            salida.write("\n")
+        
+
+    salida.close()
+    entrada.close()
---
+++ RAWDATA/INBOUND/SCRIPTS/Timer.py
@@ -0,0 +1,9 @@
+import CSVtoTXT
+import time
+
+def timerScript():
+    CSVtoTXT.ejecucionScript()
+    time.sleep(10)
+
+while True:
+    timerScript()
