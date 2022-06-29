150d8623e112cd7dcd70dea69282d933093ec97c
Luis Correa Céspedes <luiscorreacespedes@gmail.com>
2022-06-29 04:06:34 -0400

    CSVtoTXT.py modificado

--- RAWDATA/INBOUND/SCRIPTS/CSVtoTXT.py
+++ RAWDATA/INBOUND/SCRIPTS/CSVtoTXT.py
@@ -1,6 +1,7 @@
 #Librerías
 import csv
 import os
+import hashlib
 
 def ejecucionScript():
 
@@ -8,6 +9,18 @@ def ejecucionScript():
     directorio = "../ARCHIVO_CARGA/"
     with os.scandir(directorio) as ficheros:
         ficheros = [fichero.name for fichero in ficheros if fichero.is_file() and fichero.name.endswith('.csv')]
+
+        #Obtención de hash
+        h = hashlib.sha1()
+        for elemento in ficheros:
+            file = open("../ARCHIVO_CARGA/" + elemento, 'rb')
+            chunk = 0
+            while chunk != b'':
+                chunk = file.read(1024)
+                h.update(chunk)
+            print(h.hexdigest())
+            
+        
 
     #Lectura de ficheros
     for rutas in ficheros:
